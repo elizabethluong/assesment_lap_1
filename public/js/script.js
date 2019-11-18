f2d165b6f991cf12f14ad829f20c59a53bd960a9
Elizabeth Luong <elizabeth@Belles-MacBook-Air.local>
2019-11-18 12:40:46 +0000

    toggle h1

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(() => {
   let form = document.getElementById("form");
   $("form").submit(event => {
     event.preventDefault();
+    $("h1").toggle();
     axios.post("").then(response => {
       quote = response.data;
       $("#text").css({
